cb855dac07df35609c7a751117b3cbfd73a74bd8
Zoe Guidry <oyezoey@gmail.com>
2022-10-11 16:48:16 -0500

    commit tree 418339af9ab3b77bfa308e88d0c505321e3637cd

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+const hqLocation = 42;
+const blockToFeet = 264;
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(hqLocation - pickupLocation);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * blockToFeet;
+}
+
+function distanceTravelledInFeet(pickupLocation, destination) {
+    return Math.abs(pickupLocation - destination) * blockToFeet;
+}
+
+function calculatesFarePrice(pickupLocation, destination) {
+    const distance = distanceTravelledInFeet(pickupLocation, destination);
+    if (distance <= 400) {
+        return 0;
+    }
+    else if (distance <= 2000) {
+        return (distance - 400) * 0.02;
+    }
+    else if (distance <= 2500) {
+        return 25;
+    }
+    else {
+        return 'cannot travel that far';
+    }
+}
+
